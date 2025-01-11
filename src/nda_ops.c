3f5c4ef691f64cb6d0909404bb9dcaec0855ed22
Jonathan Simpson <jjsimpso@gmail.com>
2025-01-11 14:49:42 -0500

    Cleanup ndarray_mul().

--- src/nda_ops.c
+++ src/nda_ops.c
@@ -5,6 +5,23 @@
 
 #include "ndarray.h"
 
+/*
+  allocates and returns an NDArray the same size as the input multi iterator with 
+  element size elem_bytes.
+*/
+NDArray *ndarray_new_result(NDArrayMultiIter *mit, intptr_t elem_bytes)
+{
+    uint8_t *c_data = (uint8_t*) malloc(mit->length * elem_bytes);
+
+    intptr_t dims[MAX_DIMS] = {0};
+    for(int i = 0; i <= mit->nd_m1; i++)
+    {
+	dims[i] = mit->dims_m1[i] + 1;
+    }
+    
+    return ndarray_new(mit->nd_m1 + 1, dims, elem_bytes, c_data);
+}
+
 // assume arrays contain doubles for now
 /* 
    allocates an NDArray to store the result and returns a pointer to it
@@ -19,32 +36,19 @@ NDArray *ndarray_mul(NDArray *a, NDArray *b)
     }
 
     // todo: add assert to check A and B elem_bytes are equal to sizeof(double)
-    intptr_t elem_bytes = sizeof(double);
-    uint8_t *c_data = (uint8_t*) malloc(mit->length * elem_bytes);
-
-    intptr_t dims[MAX_DIMS];
-    memcpy(dims, mit->dims_m1, sizeof(dims));
-    for(int i = 0; i <= mit->nd_m1; i++)
-    {
-	dims[i]++;
-    }
     
-    NDArray *c = ndarray_new(mit->nd_m1 + 1, dims, elem_bytes, c_data);
+    // allocate result array
+    NDArray *c = ndarray_new_result(mit, sizeof(double));
     if(!c)
     {
 	ndarray_multi_iter_free(mit);
 	return NULL;
     }
 
-    double *result = (double *) c_data;
-    double *ap, *bp;
+    double *result = (double *) NDARRAY_DATAPTR(c);
     do
     {
-	ap = (double *)MULTI_ITER_DATAPTR(mit, 0);
-	bp = (double *)MULTI_ITER_DATAPTR(mit, 1);
-	*result = *ap * *bp;
-	//*result = MULTI_ITER_DATA(mit, 0, double) * MULTI_ITER_DATA(mit, 1, double);
-	result += 1;
+	*result++ = MULTI_ITER_DATA(mit, 0, double) * MULTI_ITER_DATA(mit, 1, double);
     } while(ndarray_multi_iter_next(mit));
 
     ndarray_multi_iter_free(mit);
