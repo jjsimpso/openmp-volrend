33bb350403ccf1eb003d923c832c9f11e6691eb8
Jonathan Simpson <jjsimpso@gmail.com>
2026-08-19 23:22:32 -0400

    nda: added convolve2d C functions for entire ndarray
      - also check for results < 0 so kernels can include negative params

--- racket/convolve.rkt
+++ racket/convolve.rkt
@@ -17,9 +17,11 @@
 
 (define libvolrend (ffi-lib "../libvolrend"))
 
-(define-ndarray ndarray_convolve2d_uint8_t (_fun _NDArray-pointer _cvector _int _int _intptr _intptr -> _uint8))
+(define-ndarray ndarray_convolve2d_point_uint8_t (_fun _NDArray-pointer _cvector _int _int _intptr _intptr -> _uint8))
 
-(define-ndarray ndarray_convolve2d_vec3_uint8_t (_fun _NDArray-pointer _cvector _int _int _intptr _intptr _pointer -> _pointer))
+(define-ndarray ndarray_convolve2d_point_vec3_uint8_t (_fun _NDArray-pointer _cvector _int _int _intptr _intptr _pointer -> _pointer))
+
+(define-ndarray ndarray_convolve2d_uint8_t (_fun _NDArray-pointer _cvector _int _int -> _NDArray-pointer/null))
 
 
 (define (tensor-read-pgm path)
@@ -161,9 +163,9 @@
   (for* ([y (in-range 2 (- height 2))]
          [x (in-range 2 (- width 2))])
     #;(when (and (< y 5) (< x 5))
-      (printf "~ax~a ~a -> ~a " x y (ndarray-ref (tensor-ndarray t) _uint8 y x) (ndarray_convolve2d_uint8_t (tensor-ndarray t2) kernel 3 3 x y)))
-    #;(ptr-set! cursor _uint8 (ndarray_convolve2d_uint8_t (tensor-ndarray t) kernel 3 3 x y))
-    (ndarray-set! (tensor-ndarray t2) _uint8 y x (ndarray_convolve2d_uint8_t (tensor-ndarray t) kernel 3 3 x y))
+      (printf "~ax~a ~a -> ~a " x y (ndarray-ref (tensor-ndarray t) _uint8 y x) (ndarray_convolve2d_point_uint8_t (tensor-ndarray t2) kernel 3 3 x y)))
+    #;(ptr-set! cursor _uint8 (ndarray_convolve2d_point_uint8_t (tensor-ndarray t) kernel 3 3 x y))
+    (ndarray-set! (tensor-ndarray t2) _uint8 y x (ndarray_convolve2d_point_uint8_t (tensor-ndarray t) kernel 3 3 x y))
     #;(when (and (< y 5) (< x 5))
       (printf " actual=~a~n" (ndarray-ref (tensor-ndarray t2) _uint8 y x)))
     #;(ptr-add! cursor 1))
@@ -182,7 +184,7 @@
   #;(define cursor (ptr-add (NDArray-dataptr (tensor-ndarray t2)) 0))
   (for* ([y (in-range 2 (- height 2))]
          [x (in-range 2 (- width 2))])
-    (ndarray_convolve2d_vec3_uint8_t (tensor-ndarray t) kernel 3 3 x y rgb)
+    (ndarray_convolve2d_point_vec3_uint8_t (tensor-ndarray t) kernel 3 3 x y rgb)
     #;(when (and (< y 5) (< x 5))
         (printf "~ax~a ~a -> ~a ~a ~a " x y (ndarray-ref (tensor-ndarray t) _uint8 y x) (ptr-ref rgb _uint8 0) (ptr-ref rgb _uint8 1) (ptr-ref rgb _uint8 2)))
     ;(tset! t2 y x 0 (ptr-ref rgb _uint8 0))
@@ -194,3 +196,12 @@
   t2)
 
 ;(image-smooth (tensor-read-pgm "../data/dosboxes.pgm"))
+
+(define (image-smooth-fast t)
+  (define kernel (cvector _double
+                          (exact->inexact 1/9) (exact->inexact 1/9) (exact->inexact 1/9)
+                          (exact->inexact 1/9) (exact->inexact 1/9) (exact->inexact 1/9)
+                          (exact->inexact 1/9) (exact->inexact 1/9) (exact->inexact 1/9)))
+  (make-tensor (tshape t) (ndarray_convolve2d_uint8_t (tensor-ndarray t) kernel 3 3) #:ctype _uint8))
+
+;(image-smooth-fast (tensor-read-pgm "../data/dosboxes.pgm"))
--- src/nda_convol.c
+++ src/nda_convol.c
@@ -10,9 +10,9 @@
 
 
 /* 
-   the kernel must only include positive values 
+
 */
-uint8_t ndarray_convolve2d_uint8_t(NDArray *base, double *kernel, int kw, int kh, intptr_t x, intptr_t y)
+uint8_t ndarray_convolve2d_point_uint8_t(NDArray *base, double *kernel, int kw, int kh, intptr_t x, intptr_t y)
 {
     intptr_t w = base->dims[1];
     uint8_t (*data)[w];
@@ -31,10 +31,10 @@ uint8_t ndarray_convolve2d_uint8_t(NDArray *base, double *kernel, int kw, int kh
 	}
     }
 
-    return (val < UINT8_MAX) ? (uint8_t)val : UINT8_MAX;
+    return (val < UINT8_MAX) ? (uint8_t)((val < 0.0) ? 0 : val) : UINT8_MAX;
 }
 
-uint8_t *ndarray_convolve2d_vec3_uint8_t(NDArray *base, double *kernel, int kw, int kh, intptr_t x, intptr_t y, uint8_t *retval)
+uint8_t *ndarray_convolve2d_point_vec3_uint8_t(NDArray *base, double *kernel, int kw, int kh, intptr_t x, intptr_t y, uint8_t *retval)
 {
     intptr_t w = base->dims[1];
     uint8_t (*data)[w][3];
@@ -55,9 +55,123 @@ uint8_t *ndarray_convolve2d_vec3_uint8_t(NDArray *base, double *kernel, int kw, 
 	}
     }
 
-    retval[0] = (val[0] < UINT8_MAX) ? (uint8_t)val[0] : UINT8_MAX;
-    retval[1] = (val[1] < UINT8_MAX) ? (uint8_t)val[1] : UINT8_MAX;
-    retval[2] = (val[2] < UINT8_MAX) ? (uint8_t)val[2] : UINT8_MAX;
+    retval[0] = (val[0] < UINT8_MAX) ? (uint8_t)((val[0] < 0.0) ? 0 : val[0]) : UINT8_MAX;
+    retval[1] = (val[1] < UINT8_MAX) ? (uint8_t)((val[1] < 0.0) ? 0 : val[1]) : UINT8_MAX;
+    retval[2] = (val[2] < UINT8_MAX) ? (uint8_t)((val[2] < 0.0) ? 0 : val[2]) : UINT8_MAX;
     
     return retval;
+}
+
+NDArray *ndarray_convolve2d_uint8_t(NDArray *base, double *kernel, int kw, int kh)
+{
+    intptr_t h = base->dims[0];
+    intptr_t w = base->dims[1];
+    uint8_t (*in_data)[w];
+    in_data = (uint8_t (*)[w])NDARRAY_DATAPTR(base);
+
+    NDArray *out = ndarray_new(2, (intptr_t []){h, w}, sizeof(uint8_t), NULL);
+    if(!out)
+    {
+	return NULL;
+    }
+    
+    uint8_t (*out_data)[w];
+    out_data = (uint8_t (*)[w])NDARRAY_DATAPTR(out);
+    
+    double (*k)[kw] = (double (*)[kw])kernel;
+    int jrange = kh/2;
+    int irange = kw/2;
+
+    /* handle literal edge cases where kernel extends outside of data array by clamping values */
+    /* top */
+    for(int y = 0; y < jrange; y++)
+    {
+	for(int x = irange; x < (w - irange); x++)
+	{
+	    double val = 0.0;
+	    for(int j = -jrange; j <= jrange; j++)
+	    {
+		int yidx = ((y+j) < 0) ? 0 : (y+j);
+		for(int i = -irange; i <= irange; i++)
+		{
+		    val += (double)in_data[yidx][x+i] * k[j+jrange][i+irange];
+		}
+	    }
+	    out_data[y][x] = (val < UINT8_MAX) ? (uint8_t)((val < 0.0) ? 0 : val) : UINT8_MAX;
+	}
+    }
+
+    /* bottom */
+    for(int y = (h - jrange); y < h; y++)
+    {
+	for(int x = irange; x < (w - irange); x++)
+	{
+	    double val = 0.0;
+	    for(int j = -jrange; j <= jrange; j++)
+	    {
+		int yidx = ((y+j) > (h-1)) ? (h-1) : (y+j);
+		for(int i = -irange; i <= irange; i++)
+		{
+		    val += (double)in_data[yidx][x+i] * k[j+jrange][i+irange];
+		}
+	    }
+	    out_data[y][x] = (val < UINT8_MAX) ? (uint8_t)((val < 0.0) ? 0 : val) : UINT8_MAX;
+	}
+    }
+
+    /* left */
+    for(int y = jrange; y < (h - jrange); y++)
+    {
+	for(int x = 0; x < irange; x++)
+	{
+	    double val = 0.0;
+	    for(int j = -jrange; j <= jrange; j++)
+	    {
+		for(int i = -irange; i <= irange; i++)
+		{
+		    int xidx = ((x+i) < 0) ? 0 : (x+i);
+		    val += (double)in_data[y+j][xidx] * k[j+jrange][i+irange];
+		}
+	    }
+	    out_data[y][x] = (val < UINT8_MAX) ? (uint8_t)((val < 0.0) ? 0 : val) : UINT8_MAX;
+	}
+    }
+
+    /* right */
+    for(int y = jrange; y < (h - jrange); y++)
+    {
+	for(int x = (w - irange); x < w; x++)
+	{
+	    double val = 0.0;
+	    for(int j = -jrange; j <= jrange; j++)
+	    {
+		for(int i = -irange; i <= irange; i++)
+		{
+		    int xidx = ((x+i) > (w-1)) ? (w-1) : (x+i);
+		    val += (double)in_data[y+j][xidx] * k[j+jrange][i+irange];
+		}
+	    }
+	    out_data[y][x] = (val < UINT8_MAX) ? (uint8_t)((val < 0.0) ? 0 : val) : UINT8_MAX;
+	}
+
+    }
+    
+    /* the rest */
+    for(int y = jrange; y < (h - jrange); y++)
+    {
+	for(int x = irange; x < (w - irange); x++)
+	{
+	    double val = 0.0;
+	    for(int j = -jrange; j <= jrange; j++)
+	    {
+		for(int i = -irange; i <= irange; i++)
+		{
+		    val += (double)in_data[y+j][x+i] * k[j+jrange][i+irange];
+		}
+	    }
+	    out_data[y][x] = (val < UINT8_MAX) ? (uint8_t)((val < 0.0) ? 0 : val) : UINT8_MAX;
+	}
+    }
+
+    return out;
 }
